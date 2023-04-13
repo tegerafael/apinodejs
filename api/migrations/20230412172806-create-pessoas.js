ad2172618117c670aef425ac8ca7660718a40a2b
Tiago Rafael <tiagorafael777@gmail.com>
2023-04-13 12:17:28 -0300

    3. Controladores e rotas

--- api/migrations/20230412172806-create-pessoas.js
+++ api/migrations/20230412172806-create-pessoas.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Pessoas", {
-      id_pes: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
