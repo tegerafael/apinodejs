9f386f4e7a74894f43745b6c5b6540e826c64733
Tiago Rafael <tiagorafael777@gmail.com>
2023-04-13 13:00:39 -0300

    4. CRUD com Sequelize

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,10 +1,58 @@
 const database = require("../models");
 
 class PessoaController {
-  static async listar(req, res) {
+  static async list(req, res) {
     try {
       const pessoas = await database.Pessoas.findAll();
       return res.status(200).json(pessoas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async listById(req, res) {
+    const { id } = req.params;
+    try {
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async register(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    const novaPessoa = req.body;
+    try {
+      await database.Pessoas.update(novaPessoa, { where: { id: Number(id) } });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async delete(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      return res
+        .status(200)
+        .json({ message: `Pessoa com o id ${id} excluído com sucesso` });
     } catch (error) {
       return res.status(500).json(error.message);
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,10 @@ const PessoaController = require("../controllers/PessoaController");
 
 const router = Router();
 
-router.get("/pessoas", PessoaController.listar);
+router.get("/pessoas", PessoaController.list);
+router.get("/pessoas/:id", PessoaController.listById);
+router.post("/pessoas", PessoaController.register);
+router.put("/pessoas/:id", PessoaController.update);
+router.delete("/pessoas/:id", PessoaController.delete);
 
 module.exports = router;
